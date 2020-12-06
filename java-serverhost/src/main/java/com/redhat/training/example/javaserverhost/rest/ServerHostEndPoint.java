46e4bef11d545ef2df18be322b6b332bf94ff7bd
Student User <student@workstation.lab.example.com>
2020-12-06 10:26:47 -0500

    updated version

--- java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
+++ java-serverhost/src/main/java/com/redhat/training/example/javaserverhost/rest/ServerHostEndPoint.java
@@ -20,7 +20,7 @@ public class ServerHostEndPoint {
        e.printStackTrace();
     }
 //    String msg = "I am running on server "+host+" Version 1.0 \n";
-    String msg = "I am running on server "+host+" Version 2.0 \n";
+    String msg = "I am running on server "+host+" Version 3.0 \n";
     return Response.ok(msg).build();
   }
 }
